e64d054f227f8b3145307dcd2cfe72ef9d7954b0
pinyin <bob432091@gmail.com>
2018-07-10 09:46:18 +0800

    make assume to return undefined instead of null

--- src/assume.spec.ts
+++ src/assume.spec.ts
@@ -6,7 +6,7 @@ describe(`${assume.name}`, () => {
     })
     test(`${assume.name} should call func with first param as param iff first param is defined`, () => {
         const func = (a: number) => { return a + 1}
-        expect(assume(null, func)).toBeNull()
+        expect(assume(null, func)).toBeUndefined()
         expect(assume(1, func)).toEqual(2)
         expect(assume(1, func, func)).toEqual(3)
         expect(assume(1, func, func, func, func)).toEqual(5)
--- src/assume.ts
+++ src/assume.ts
@@ -1,14 +1,12 @@
-import {nothing} from '@pinyin/types'
 import {existing} from './existing'
-import {Maybe} from './Maybe'
 
 export type FromTo<A, B> = (a: NonNullable<A>) => B
 
-export function assume<A, B>(a: A, func: FromTo<A, B>): Maybe<B>
-export function assume<A, B, C>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>): Maybe<C>
-export function assume<A, B, C, D>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>): Maybe<D>
-export function assume<A, B, C, D, E>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>, f4: FromTo<D, E>): Maybe<E>
-export function assume<A, B, C, D, E, F>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>, f4: FromTo<D, E>, f5: FromTo<E, F>): Maybe<F>
-export function assume<A, B>(a: A, ...funcs: Array<FromTo<any, any>>): Maybe<B> {
-    return funcs.reduce((acc, curr) => existing(acc) ? curr(acc) : nothing, a)
+export function assume<A, B>(a: A, func: FromTo<A, B>): undefined | B
+export function assume<A, B, C>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>): undefined | C
+export function assume<A, B, C, D>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>): undefined | D
+export function assume<A, B, C, D, E>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>, f4: FromTo<D, E>): undefined | E
+export function assume<A, B, C, D, E, F>(a: A, f1: FromTo<A, B>, f2: FromTo<B, C>, f3: FromTo<C, D>, f4: FromTo<D, E>, f5: FromTo<E, F>): undefined | F
+export function assume<A, B>(a: A, ...funcs: Array<FromTo<any, any>>): undefined | B {
+    return funcs.reduce((acc, curr) => existing(acc) ? curr(acc) : undefined, a)
 }
